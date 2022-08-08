88f64fc42b001e95e6ac95ae09395bbf8221b910
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-08 09:28:41 -0600

    tested to the end of iteration 1 working

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -28,7 +28,15 @@ RSpec.describe Vendor do
 
   it 'can stock an item and quantity and then show inventory' do
     @vendor.stock(@item1, 30)
-    expect(@vendor.stock).to eq({@item1 => 30})
+    expect(@vendor.inventory).to eq({@item1 => 30})
+    expect(@vendor.check_stock(@item1)).to eq 30
   end
+
+  it 'can check stock and quantity on multiple items' do
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+    expect(@vendor.inventory).to eq({@item1 => 25, @item2 => 12})
+  end
+
 
 end
