f014c76c8c4ac2e4d3ac29053e33a78f9be11bb7
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-08 09:07:00 -0600

    it exists test

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,5 @@
 class Item
+
+
+
 end
---
+++ spec/item_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require './lib/item'
+
+RSpec.describe Item do
+
+  before :each do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+    expect(@item2).to be_a(Item)
+  end
+end
