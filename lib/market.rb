2ca438f39c3c14cfa37fc2800ab8db68db3213d4
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-08 09:30:42 -0600

    Market exists test written

--- lib/market.rb
+++ lib/market.rb
@@ -1,2 +1,5 @@
 class Market
+
+
+
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,1 +1,16 @@
 require 'spec_helper'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+
+  before :each do
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  it 'exists' do
+    expect(@market).to be_a(Market)
+  end
+
+end
