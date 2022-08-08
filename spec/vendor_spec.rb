f34e69750a771b1b3a1fd747af8fa79c22079db5
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-08 09:11:15 -0600

    test for existance in vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+
+  before :each do
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  it 'exists' do
+    expect(@vendor).to be_a(Vendor)
+  end
+
+end
