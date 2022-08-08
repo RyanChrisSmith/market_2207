d15e2a7c79e61e3370dd9f56bedeb10d2ee43590
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-08 10:12:56 -0600

    test for total inventory written

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -63,6 +63,20 @@ RSpec.describe Market do
     @market.add_vendor(@vendor3)
     expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
     expect(@market.vendors_that_sell(@item3)).to eq([@vendor2])
+    expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+  end
+
+  it 'can show total inventory of all vendors' do
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expect(@market.total_inventory).to eq({})
   end
 
 end
